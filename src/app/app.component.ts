3cb9f666180601c5a36fbf3f37e8763e4191cb29
Igor Aguiar <desenvigor@gmail.com>
2021-09-01 08:15:35 -0300

    create add contact event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'contatos';
+  mostrandoModal = false;
+
+
+  funcao(){
+    this.mostrandoModal = true
+  }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  @Output() emissor:EventEmitter<null> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  mostrarModalClick(): void {
+    console.log('Clicou add contato')
+    this.emissor.emit();
+  }
 }
